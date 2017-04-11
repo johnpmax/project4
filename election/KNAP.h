60c74cefb8b654921dc12ba4d73e800e37ebe0ca
johnpmax <johnpmax@umich.edu>
2017-04-10 23:11:01 -0400

    new knap solution

--- election/KNAP.h
+++ election/KNAP.h
@@ -50,10 +50,52 @@ public:
     
     //solve knapsack
     void solve(){
-        //std::cout << "least: " << leastCost << '\n';
-        std::vector<int> visited;
-        //totalVotes = knapsack(budget, numStates, visited);
-        //std::cout << "votes: " << totalVotes << '\n';
+        std::vector<std::vector<int>> graph(int(budget)+1, std::vector<int>(numStates+1));
+        std::vector<int> used(int(budget)+1, 0);
+        
+        for(int x = 0; x <= budget; ++x){
+            for(int y = 0; y <= numStates; ++y){
+                if(x == 0 || y == 0){
+                    graph[x][y] = 0;
+                }else if(states[y-1].cost <= y){
+                    int a = states[y-1].votes + graph[x-int(states[x-1].cost)-1][y];
+                    int b = graph[x][y-1];
+                    if(a >= b){
+                        graph[x][y] = a;
+                        used[x] = states[y-1].index;
+                    }else{
+                        graph[x][y] = b;
+                    }
+                }else{
+                    graph[x][y] = graph[x][y-1];
+                }
+            }
+        }
+        
+        totalVotes = graph[int(budget)+1][numStates];
+        /*int chart[numStates+1][int(budget)+2];
+        
+        for(int x = 0; x <= numStates; ++x){
+            for(double y = 0; y <= budget+1; ++y){
+                if(x == 0 || x ==0){
+                    chart[x][int(y)] = 0;
+                }else if(states[x-1].cost <= y){
+                    int a = states[x-1].votes + chart[x-1][int(y-states[x-1].cost)];
+                    int b = chart[x-1][int(y)];
+                    if(a >= b){
+                        chart[x][int(y)] = a;
+                    }else{
+                        chart[x][int(y)] = b;
+                    }
+                    
+                    //chart[x][int(y)] = maxVotes(states[x-1].votes +
+                    //chart[x-1][int(y-states[x-1].cost)], chart[x-1][int(y)]);
+                }else{
+                    chart[x][int(y)] = chart[x-1][int(y)];
+                }
+            }
+        }
+        totalVotes = chart[numStates][int(budget)];*/
     }
     /*
     //recursively solve knapsack problem
